2135b01fc573a28697684db234cd9b0d038f9ea1
CtkGHoSt <39295159+CtkGHoSt@users.noreply.github.com>
2020-03-27 02:32:54 +0800

    Update 3hds_crwaler.py

--- 3hds_crwaler.py
+++ 3hds_crwaler.py
@@ -84,14 +84,13 @@ def main():
     
     link = new_post_link()
     get_post_res = get_3hds_post(link)
-    print(link)
-    print(get_post_res)
     if get_post_res['status'] is False:
         gslj_bot.send_message(TG_CHAT_ID, 
         '3和大神bot运行错误:{}'.format(get_post_res['msg']))
         return
     for item in get_post_res['result']:
         # print(item['time'], item['time'] + datetime.timedelta(hours=10))
+        print(item['time'] + datetime.timedelta(hours=1), '---', datetime.datetime.now(tz).replace(tzinfo=None))
         if item['time'] + datetime.timedelta(hours=1) > datetime.datetime.now(tz).replace(tzinfo=None):
             print(item)
             gslj_bot.send_message(CHANNEL_ID, 
